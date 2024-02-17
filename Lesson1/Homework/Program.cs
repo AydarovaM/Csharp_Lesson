54e3413d36c28ae106c823c52dcea372286b13e6
Marina <7404@list.ru>
2024-02-17 15:59:54 +0300

    add Lesson1:Homework

---
+++ Lesson1/Homework/Program.cs
@@ -0,0 +1,9 @@
+﻿//Вычислить значение a*b/(c+d), где а, b, c, d  некоторые целые числа. РЕзультат вывести на экран.
+
+int a = 3;
+int b = 4;
+int c = 1;
+int d = 2;
+int sum = c + d;
+int result = a * b / sum;
+Console.WriteLine(result);
